46c08a91fe9016a149808e77359061bdb09c35cb
Oleg Nenashev <o.v.nenashev@gmail.com>
2018-03-05 12:08:23 +0100

    Add command-line help + “-demo” mode

--- war-packager-cli/src/main/java/io/jenkins/tools/warpackager/cli/CliOptions.java
+++ war-packager-cli/src/main/java/io/jenkins/tools/warpackager/cli/CliOptions.java
@@ -2,28 +2,40 @@ package io.jenkins.tools.warpackager.cli;
 
 import org.kohsuke.args4j.Option;
 
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
 import java.io.File;
 
 public class CliOptions {
 
-    @Option(name = "configPath")
+    @Option(name = "-configPath", usage = "Path to the configuration YAML. See the tool's README for format")
     public File configPath;
 
-    @Option(name = "tmpDir")
+    @Option(name = "-tmpDir", usage = "Temporary directory for generated files and the output WAR. Defaults to 'tmp'")
     public File tmpDir;
 
-    @Option(name = "version")
+    @Option(name = "-version", usage = "Version of WAR to be set. Defaults to '1.0-SNAPSHOT'")
     public String version;
 
+    @Option(name = "-demo", usage = "Enables demo mode with predefined config file")
+    public boolean demo;
+
+    @CheckForNull
     public File getConfigPath() {
         return configPath;
     }
 
+    @Nonnull
     public File getTmpDir() {
         return tmpDir != null ? tmpDir : new File("tmp");
     }
 
+    @Nonnull
     public String getVersion() {
         return version != null ? version : "1.0-SNAPSHOT";
+    }
+
+    public boolean isDemo() {
+        return demo;
     }
 }
--- war-packager-cli/src/main/java/io/jenkins/tools/warpackager/cli/Main.java
+++ war-packager-cli/src/main/java/io/jenkins/tools/warpackager/cli/Main.java
@@ -12,13 +12,23 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         CliOptions options = new CliOptions();
         CmdLineParser p = new CmdLineParser(options);
+        if (args.length == 0) {
+            System.out.println("Usage: java -jar war-packager-cli.jar -configPath=mywar.yml [-version=1.0-SNAPSHOT] [-tmpDir=tmp]\n");
+            p.printUsage(System.out);
+            return;
+        }
+
         try {
             p.parseArgument(args);
         } catch (CmdLineException ex) {
+            p.printUsage(System.out);
             throw new IOException("Failed to read command-line arguments", ex);
         }
 
-        Config cfg = Config.loadConfig(options.getConfigPath());
+        if (options.getConfigPath() == null && !options.isDemo()) {
+            throw new IOException("-configPath or -demo must be defined");
+        }
+        Config cfg = Config.loadConfig(options.isDemo() ? null : options.getConfigPath());
 
         Builder bldr = new Builder(options, cfg);
         bldr.build();
