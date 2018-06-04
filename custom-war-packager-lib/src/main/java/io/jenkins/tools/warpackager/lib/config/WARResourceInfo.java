2157dc3b6bc339ebcd77da164eea6220919b4415
Oleg Nenashev <o.v.nenashev@gmail.com>
2018-06-04 15:59:23 +0200

    [JENKINS-51656] - FindBugs

--- custom-war-packager-lib/src/main/java/io/jenkins/tools/warpackager/lib/config/WARResourceInfo.java
+++ custom-war-packager-lib/src/main/java/io/jenkins/tools/warpackager/lib/config/WARResourceInfo.java
@@ -1,10 +1,13 @@
 package io.jenkins.tools.warpackager.lib.config;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 /**
  * Abstraction for all resources being injected into WARs.
  * @author Oleg Nenashev
  * @since TODO
  */
+@SuppressFBWarnings(value = "UWF_UNWRITTEN_PUBLIC_OR_PROTECTED_FIELD", justification = "JSON Deserialization")
 public abstract class WARResourceInfo {
     public String id;
     public SourceInfo source;
