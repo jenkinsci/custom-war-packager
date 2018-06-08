4bcec55fd0c289208ef054e261a838a39315a7f0
Oleg Nenashev <o.v.nenashev@gmail.com>
2018-06-08 15:56:06 +0200

    [JENKINS-51778] - Add Job self-configuration

--- demo/external-logging-elasticsearch/src/main/groovy/3_Jobs.groovy
+++ demo/external-logging-elasticsearch/src/main/groovy/3_Jobs.groovy
@@ -10,21 +10,24 @@ if (!Boolean.getBoolean("io.jenkins.demo.external-task-logging-elk.enabled")) {
 
 println("-- Creating Jobs")
 //TODO: Classes do not work here, so some copy-paste for now
-/*
-WorkflowJob project1 = Jenkins.instance.createProject(WorkflowJob.class, "Demo_master")
-project1.definition = new CpsFlowDefinition(
-    "echo \"ping -c 20 google.com\"",
-    true // Sandbox
-)
-project1.save()
 
-WorkflowJob project2 = Jenkins.instance.createProject(WorkflowJob.class, "Demo_agent")
-project2.definition = new CpsFlowDefinition(
-    "node('agent') {" +
-    "  echo \"Hello, world!\"" +
-    "  sh \"ping -c 20 google.com\"" +
-    "}",
-    true // Sandbox
-)
-project2.save()
-*/
+if(Jenkins.instance.getItem("Demo_master") == null) {
+    WorkflowJob project1 = Jenkins.instance.createProject(WorkflowJob.class, "Demo_master")
+    project1.definition = new CpsFlowDefinition(
+        "echo \"ping -c 20 google.com\"",
+        true // Sandbox
+    )
+    project1.save()
+}
+
+if(Jenkins.instance.getItem("Demo_agent") == null) {
+    WorkflowJob project2 = Jenkins.instance.createProject(WorkflowJob.class, "Demo_agent")
+    project2.definition = new CpsFlowDefinition(
+        "node('agent') {" +
+            "  echo \"Hello, world!\"" +
+            "  sh \"ping -c 20 google.com\"" +
+            "}",
+        true // Sandbox
+    )
+    project2.save()
+}
