e08a021c52d647115136fa93f34bd466313c3b7e
Olivier Dagenais <odagenais@kinaxis.com>
2018-11-14 09:42:37 -0500

    fix: support launching batch files on Windows
    Maven's "executable" is mvn.bat so by prefixing with `cmd /C` we
    allow "executables" supported by the Command Interpreter.

--- custom-war-packager-lib/src/main/java/io/jenkins/tools/warpackager/lib/util/SystemCommandHelper.java
+++ custom-war-packager-lib/src/main/java/io/jenkins/tools/warpackager/lib/util/SystemCommandHelper.java
@@ -6,6 +6,8 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 /**
  * @author Oleg Nenashev
@@ -13,10 +15,21 @@ import java.nio.charset.Charset;
  */
 public class SystemCommandHelper {
 
+    // https://stackoverflow.com/a/228499
+    private static final String OS_NAME = System.getProperty("os.name");
+    private static final boolean IS_WINDOWS = OS_NAME != null && OS_NAME.startsWith("Windows");
+    //https://stackoverflow.com/a/17120829
+    private static final String[] WINDOWS_PROCESS_ARGS_PREFIX = {"cmd.exe", "/C"};
+
     private SystemCommandHelper() {}
 
     private static ProcessBuilder createProcessBuilder(final String[] args) {
-        return new ProcessBuilder(args);
+        String[] combined = args;
+        if (IS_WINDOWS) {
+            combined = Stream.concat(Arrays.stream(WINDOWS_PROCESS_ARGS_PREFIX), Arrays.stream(args))
+                    .toArray(String[]::new);
+        }
+        return new ProcessBuilder(combined);
     }
 
     public static void processFor(File buildDir, String ... args) throws IOException, InterruptedException {
