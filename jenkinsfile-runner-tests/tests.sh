a04afbd1a3f80c3d2a0012f649c0efc2f820412d
Kevin Earls <kearls@kevinearls.com>
2019-02-15 12:49:04 +0100

    [JENKINS-54352] Update Jenkins versions, fix resource paths, use newer test methods properly

--- jenkinsfile-runner-tests/tests.sh
+++ jenkinsfile-runner-tests/tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 set -e
 
 current_directory=$(pwd)
@@ -23,18 +23,13 @@ oneTimeSetUp() {
 # Use a JCasC configuration to build the JFR image, then get the system message that was set in
 # casc.yml to verify that it was applied.
 #
-# TODO for this and other tests we are copying casc.yml to the current directory so that it is
-# referenced properly in packager-config-yml.  It would be better to put an environment variable
-# or placeholder in packager-config.yml and update that with the real location before running.
-#
 test_cwp_casc_simple() {
-  cp $current_directory/test_resources/test_cwp_casc_simple/casc.yml .
   jfr_tag=$(execute_cwp_jar_and_generate_docker_image "$working_directory" "${cwp_jar}" "$version" "$current_directory/test_resources/test_cwp_casc_simple/packager-config.yml" "$jenkinsfile_runner_tag" | grep 'Successfully tagged')
   execution_should_success "$?" "$jenkinsfile_runner_tag" "$jfr_tag"
 
-  result=$(run_jfr_docker_image_with_jfr_options "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_cwp_casc_simple/Jenkinsfile" "--no-sandbox")
-  jenkinsfile_execution_should_succeed "$?" "$result"
-  assertContains "Should contain the system message configured by CasC" "$result" "Jenkins configured automatically by Jenkins Configuration as Code Plugin"
+  run_jfr_docker_image_with_jfr_options "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_cwp_casc_simple/Jenkinsfile" "--no-sandbox"
+  jenkinsfile_execution_should_succeed "$?"
+  logs_contains "Jenkins configured automatically by Jenkins Configuration as Code Plugin"
 }
 
 #
@@ -43,41 +38,37 @@ test_cwp_casc_simple() {
 # instance, so I am doing that in the Jenkinsfile.
 #
 test_cwp_jcasc_with_tool() {
-  cp $current_directory/test_resources/test_cwp_casc_with_tool/casc.yml .
   jfr_tag=$(execute_cwp_jar_and_generate_docker_image "$working_directory" "${cwp_jar}" "$version" "$current_directory/test_resources/test_cwp_casc_with_tool/packager-config.yml" "$jenkinsfile_runner_tag" | grep 'Successfully tagged')
   execution_should_success "$?" "$jenkinsfile_runner_tag" "$jfr_tag"
 
-  result=$(run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_cwp_casc_with_tool/Jenkinsfile")
-  jenkinsfile_execution_should_succeed "$?" "$result"
-  assertContains "Should contain the correct maven version" "$result" "Apache Maven 3.3.3"
+  run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_cwp_casc_with_tool/Jenkinsfile"
+  jenkinsfile_execution_should_succeed "$?"
+  logs_contains "Apache Maven 3.3.3"
 }
 
 #
 # Use a JCasC configuration to build the JFR image with a global library.  Then try to
 # reference methods in that library
 #
-#
 test_jcasc_with_pipeline_library() {
-  cp $current_directory/test_resources/test_cwp_casc_with_pipeline_library/casc.yml .
   jfr_tag=$(execute_cwp_jar_and_generate_docker_image "$working_directory" "${cwp_jar}" "$version" "$current_directory/test_resources/test_cwp_casc_with_pipeline_library/packager-config.yml" "$jenkinsfile_runner_tag" | grep 'Successfully tagged')
   execution_should_success "$?" "$jenkinsfile_runner_tag" "$jfr_tag"
 
-  result=$(run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_cwp_casc_with_pipeline_library/Jenkinsfile")
-  jenkinsfile_execution_should_succeed "$?" "$result"
-  assertContains "Should contain calls to awesome-lib" "$result" "On Jenkins Infra? false"
+  run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_cwp_casc_with_pipeline_library/Jenkinsfile"
+  jenkinsfile_execution_should_succeed "$?"
+  logs_contains "On Jenkins Infra? false"
 }
 
 #
-#
+# Test building Jenkinsfile-runner with a simple groovy hook
 #
 test_cwp_with_groovy_hooks() {
-  cp $current_directory/test_resources/test_cwp_with_groovy_hooks/init.groovy .
   jfr_tag=$(execute_cwp_jar_and_generate_docker_image "$working_directory" "${cwp_jar}" "$version" "$current_directory/test_resources/test_cwp_with_groovy_hooks/packager-config.yml" "$jenkinsfile_runner_tag" | grep 'Successfully tagged')
   execution_should_success "$?" "$jenkinsfile_runner_tag" "$jfr_tag"
 
-  result=$(run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_cwp_with_groovy_hooks/Jenkinsfile")
-  jenkinsfile_execution_should_succeed "$?" "$result"
-  assertContains "Should contain calls to awesome-lib" "$result" "System configuration by Groovy Hooks"
+  run_jfr_docker_image "$jenkinsfile_runner_tag" "$current_directory/test_resources/test_cwp_with_groovy_hooks/Jenkinsfile"
+  jenkinsfile_execution_should_succeed "$?"
+  logs_contains "System configuration by Groovy Hooks"
 }
 
 
