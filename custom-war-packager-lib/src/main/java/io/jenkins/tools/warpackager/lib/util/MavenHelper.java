77a6a184c9f1c80d7964079f00566857c6243b85
Francisco Javier Fernandez Gonzalez <fjfernandez@cloudbees.com>
2019-02-27 18:31:06 +0100

    [JENKINS-55568] Fix error in commit

--- custom-war-packager-lib/src/main/java/io/jenkins/tools/warpackager/lib/util/MavenHelper.java
+++ custom-war-packager-lib/src/main/java/io/jenkins/tools/warpackager/lib/util/MavenHelper.java
@@ -70,7 +70,7 @@ public class MavenHelper {
     }
 
     private static String getOsSpecificMavenCommand() {
-        String mvnCmd = "/home/fcojfernandez/Desarrollo/apache-maven-3.5.4/bin/mvn";
+        String mvnCmd = "mvn";
 
         String osName = System.getProperty("os.name");
         if(osName != null && osName.toLowerCase().contains("windows")) {
